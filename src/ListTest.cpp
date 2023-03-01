21dece32172909727bfd1cf3a8790c88cce1acc9
alexxRT <alekseev.aa@phystech.edu>
2023-03-02 01:34:49 +0300

    test file for release version

--- src/ListTest.cpp
+++ src/ListTest.cpp
@@ -1,27 +1,42 @@
 #include "../lib/List.h"
+#include <stdio.h>
 
 
 
 int main ()
 {
-    InitDumpFile ();
+    
     my_list list = {};
 
     ListInit (&list, 10);
+    printf ("\n1) LIST INITED\n");
 
-    list_data_t a = 5;
-    list_data_t b = 6;
+    for (int i = 0; i < 11; i ++)
+        ListInsertRight (&list, 0, i);
 
-    ListAdd (&list, a);
-    ListAdd (&list, b);
+    printf ("2) INSERTATION COMPLETE 11 ELEMS\n");
 
-    // ListPop (&list, &b);
-    // ListPop (&list, &a);
+    ListDeleteIndex (&list, 7);
+    ListDeleteIndex (&list, 4);
+    ListDeleteIndex (&list, 10);
 
-    ListTextDump (&list);
+    printf ("3) 3 ELEMS DELETED BY INDEX\n");
+
+    MakeListGreatAgain (&list);
+    
+    printf ("4) LIST LINIARIZED\n");
+
+
+    for (int i = 0; i < 5; i ++)
+    {
+        int err = ListDelete (&list, 1);
+    }
+
+    printf ("5) 5 ELEMS DELETED\n");
 
     ListDestroy (&list);
-    DestroyDumpFile ();
+
+    printf ("6) LIST DESTROYED\n\n");
 
     return 0;
 }
